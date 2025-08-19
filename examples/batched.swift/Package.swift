9b748f748e1249af4db1070564efdd9daa420bb1
rei <htnfts@gmail.com>
2025-08-19 16:30:06 +0900

    no message

--- examples/batched.swift/Package.swift
+++ examples/batched.swift/Package.swift
@@ -5,10 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "llama-batched-swift",
-    platforms: [.macOS(.v12)],
-    dependencies: [
-        .package(name: "llama", path: "../../"),
-    ],
+    platforms: [
+    .macOS(.v12),
+    .iOS(.v15),
+    .watchOS(.v4),
+    .tvOS(.v12)
+],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
